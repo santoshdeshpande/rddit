6a5f58bc9e5fa95bbc148dd9f67450671f733382
Santosh Suresh <santosh.suresh@jifflenow.com>
2014-12-12 07:45:36 +0530

    Added specs for controllers

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,4 +3,8 @@ FactoryGirl.define do
     sequence(:title) {|n| "link_title_#{n}"}
     sequence(:url) {|n| "link_url_#{n}"}
   end
+
+  factory :invalid_link, parent: :link do | l |
+    l.title nil
+  end
 end
