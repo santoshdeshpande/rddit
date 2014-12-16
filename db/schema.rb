e5a0e98ad37182642d67917d1bd82910709cef8c
Santosh Suresh <santoshdeshpande@gmail.com>
2014-12-16 03:36:30 +0000

    Facebook login added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215124320) do
+ActiveRecord::Schema.define(version: 20141216023341) do
 
   create_table "comments", force: true do |t|
     t.integer  "link_id"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20141215124320) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
